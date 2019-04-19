e8c331aa8c36343e24c28f5eedb9e82af9e878bf
Gabriel Martins <gabrielmartinssantana@hotmail.com>
2019-04-19 18:03:30 -0300

    Inicia versionamento

---
+++ src/main/java/br/ufjf/dcc193/trb01/DemoApplication.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc193.trb01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufjf/dcc193/trb01/MainApplication.java
@@ -0,0 +1,15 @@
+package br.ufjf.dcc193.trb01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+		System.out.println("Hello World!");
+		
+	}
+
+}
