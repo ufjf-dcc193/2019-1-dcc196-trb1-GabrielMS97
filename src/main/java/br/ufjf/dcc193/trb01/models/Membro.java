30fa2edfb46c5e674a5a9cd4cfffdf3fb23fb31b
Gabriel Martins <gabrielmartinssantana@hotmail.com>
2019-04-19 19:47:34 -0300

    Cria classe Membro

---
+++ src/main/java/br/ufjf/dcc193/trb01/models/Membro.java
@@ -0,0 +1,59 @@
+package br.ufjf.dcc193.trb01.models;
+
+import java.time.LocalDate;
+import javax.persistence.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+public class Membro {
+    private String nome;
+    private String funcao;
+    private String email;
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private LocalDate entrada;
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private LocalDate saida;
+
+    public Membro(){
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getFuncao(){
+        return funcao;
+    }
+
+    public void setFuncao(String funcao){
+        this.funcao = funcao;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public LocalDate getEntrada(){
+        return entrada;
+    }
+
+    public void setEntrada(LocalDate entrada){
+        this.entrada = entrada;
+    }
+
+    public LocalDate getSaida(){
+        return saida;
+    }
+
+    public void setSaida(LocalDate saida){
+        this.saida = saida;
+    }
+}
